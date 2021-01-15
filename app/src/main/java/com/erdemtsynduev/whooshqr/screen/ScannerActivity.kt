d816cb08d93625fe0e30fd764518829730b9523d
Erdem Tsynduev <erdemtsynduev@gmail.com>
2021-01-15 10:19:49 +0300

    feat: Add Logic scan QR

---
+++ app/src/main/java/com/erdemtsynduev/whooshqr/screen/ScannerActivity.kt
@@ -0,0 +1,97 @@
+package com.erdemtsynduev.whooshqr.screen
+
+import android.graphics.Rect
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.Window
+import android.widget.FrameLayout
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
+import com.erdemtsynduev.whooshqr.R
+import com.huawei.hms.hmsscankit.RemoteView
+import com.huawei.hms.ml.scan.HmsScan
+
+class ScannerActivity : AppCompatActivity() {
+
+    var frameLayout: FrameLayout? = null
+    var remoteView: RemoteView? = null
+
+    var screenWidth = 0
+    var screenHeight = 0
+
+    private val scanFrameSize = 240
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        setContentView(R.layout.activity_scanner)
+
+        frameLayout = findViewById(R.id.rim)
+
+        //1. Obtain the screen density to calculate the viewfinder's rectangle.
+        val dm = resources.displayMetrics
+        val density = dm.density
+        //2. Obtain the screen size.
+        //2. Obtain the screen size.
+        screenWidth = resources.displayMetrics.widthPixels
+        screenHeight = resources.displayMetrics.heightPixels
+
+        val scanFrameSize = (scanFrameSize * density).toInt()
+
+        //3. Calculate the viewfinder's rectangle, which in the middle of the layout.
+        //Set the scanning area. (Optional. Rect can be null. If no settings are specified, it will be located in the middle of the layout.)
+
+        //3. Calculate the viewfinder's rectangle, which in the middle of the layout.
+        //Set the scanning area. (Optional. Rect can be null. If no settings are specified, it will be located in the middle of the layout.)
+        val rect = Rect()
+        rect.left = screenWidth / 2 - scanFrameSize / 2
+        rect.right = screenWidth / 2 + scanFrameSize / 2
+        rect.top = screenHeight / 2 - scanFrameSize / 2
+        rect.bottom = screenHeight / 2 + scanFrameSize / 2
+
+        //Initialize the RemoteView instance, and set callback for the scanning result.
+        remoteView = RemoteView.Builder().setContext(this).setBoundingBox(rect).setFormat(HmsScan.ALL_SCAN_TYPE).build()
+        // Subscribe to the scanning result callback event.
+        // Subscribe to the scanning result callback event.
+        remoteView?.setOnResultCallback { result -> //Check the result.
+            if (result != null && result.isNotEmpty() && result[0] != null && !TextUtils.isEmpty(result[0].getOriginalValue())) {
+                //TODO ADD GET QR
+            }
+        }
+        // Load the customized view to the activity.
+        // Load the customized view to the activity.
+        remoteView?.onCreate(savedInstanceState)
+        val params = FrameLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT)
+        frameLayout?.addView(remoteView, params)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        remoteView?.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        remoteView?.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        remoteView?.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        remoteView?.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        remoteView?.onDestroy()
+    }
+
+    companion object {
+        const val SCAN_RESULT = "scanResult"
+        const val REQUEST_CODE_PHOTO = 0X1113
+    }
+}
